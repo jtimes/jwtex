e981792ba10fcceb1abea7e302c959f811fbd003
JHIL <time1440@hanmail.net>
2024-09-10 17:05:00 +0900

    init

---
+++ src/main/java/com/postinforg/JwtexApplication.java
@@ -0,0 +1,14 @@
+package com.postinforg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+public class JwtexApplication
+{
+  public static void main(String[] args) {
+	  SpringApplication.run(com.postinforg.JwtexApplication.class, args);
+  }
+}
